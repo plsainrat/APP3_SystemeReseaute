e41408f022be27b67486a72dc949b1b18e2f97ee
Paul SAINRAT <pl.sainrat@outlook.com>
2017-10-02 09:45:12 -0400

    maintenant ca compile

--- code/EndDevice2.cpp
+++ code/EndDevice2.cpp
@@ -1,4 +1,8 @@
 #include "mbed.h"
+#include "MMA8452.h"
+
+
+Accelerometer_MMA8452 acc(p9,p10,100000);
 
 DigitalOut myled(LED1);
 DigitalOut pin8(p8);
@@ -18,74 +22,77 @@ uint8_t WR_cmd[8] = {0x7E, 0x00, 0x04, 0x08, 0x01, 0x57, 0x52, 0x4D};
 uint8_t AC_cmd[8] = {0x7E, 0x00, 0x04, 0x08, 0x01, 0x41, 0x43, 0x72};
 
 
-void sendCMD(uint8_t cmd[], int size){
+void sendCMD(uint8_t cmd[], int size)
+{
     for(int i = 0; i < size; i++) {
         com.putc(cmd[i]);
     }
 }
 
-void sendData(){
-  led1=1;
 
 
-  //envoi des données de l'accelerometre
-  uint8_t data[3]={0,0,0};
-  acc.readXYZGravity(data,data+1,data+2);
-  sendTransmitRequest(3,data);
+void sendTransmitRequest(uint16_t length,uint8_t* data)
+{
+    uint8_t checksum=0;
 
 
-
-  //envoi de la donnée du bouton
-  if(button==1){
-    data[2]=0x01;
-    sendTransmitRequest(1,data+3);
-  }
-  else{
-    data[2]=0x00;
-    sendTransmitRequest(1,data+3);
-  }
-
-
-  led1=0;
-}
-
-void sendTransmitRequest(uint_16 length,uint_8* data){
-  uint8_t cheksum=0;
-
-
-  //start
-  com.putc(0x7E);
-  //bits de poids fort du poid du message
-  com.putc((uint8_t)(length >> 8));
-  //bits de poids faible du poid du message
-  com.putc((uint8_t)(length&0xF));
-  //transmission request
-  com.putc(0x10);
-  com.putc(0x01);
-  //broadcast radius
-  com.putc(0x00);
-  //option
-  com.putc(0x00);
+    //start
+    com.putc(0x7E);
+    //bits de poids fort du poid du message
+    com.putc((uint8_t)(length >> 8));
+    //bits de poids faible du poid du message
+    com.putc((uint8_t)(length&0xF));
+    //transmission request
+    com.putc(0x10);
+    com.putc(0x01);
+    //broadcast radius
+    com.putc(0x00);
+    //option
+    com.putc(0x00);
 
 
 //on envois le message en broadcast
-  for(int i=0;i<8;i++){
-    com.put(0x00);
-  }
-  com.putc(0xFF);
-  com.putc(0xFE);
+    for(int i=0; i<8; i++) {
+        com.putc(0x00);
+    }
+    com.putc(0xFF);
+    com.putc(0xFE);
 
-  //envoie du message
-  for(int i = 0; i < lenght; i++){
-      checksum = *(data+i) + checksum;
-      com.putc(*(data+i));
-  }
-  //calcul du checksum
-  checksum=0xFF-checksum;
-  com.putc(checksum);
+    //envoie du message
+    for(int i = 0; i < length; i++) {
+        checksum = *(data+i) + checksum;
+        com.putc(*(data+i));
+    }
+    //calcul du checksum
+    checksum=0xFF-checksum;
+    com.putc(checksum);
 
 }
 
+void sendData()
+{
+    led1=1;
+
+
+    //envoi des données de l'accelerometre
+    uint8_t data[3]= {0,0,0};
+    acc.read_xyz((char*)data,(char*)data+1,(char*)data+2);
+    sendTransmitRequest(3,data);
+
+
+
+    //envoi de la donnée du bouton
+    if(button==1) {
+        data[2]=0x01;
+        sendTransmitRequest(1,data+3);
+    } else {
+        data[2]=0x00;
+        sendTransmitRequest(1,data+3);
+    }
+
+
+    led1=0;
+}
 
 void readTransmitStatus()
 {
@@ -132,10 +139,10 @@ void init()
 int main()
 {
     led1 = 1;
-    Ticker tick;
-    tick.attach(&sendData,0.5);
     init();
     led1 = 0;
     while(1) {
+        sendData();
+        wait(0.25);
     }
 }
